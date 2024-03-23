3b7c9f43a58ac5585396b72a30d1e369e0ec60fa
BoltonAthitDavies <72595767+BoltonAthitDavies@users.noreply.github.com>
2024-03-23 11:09:37 +0700

    Add tracker Bar function

--- Vision/silo_decision.py
+++ Vision/silo_decision.py
@@ -3,7 +3,7 @@ import json
 # 0 = oppenent's ball
 
         #center     #left     #right    #leftleft  #rightright
-silo = [[1,0,1],    [1,0],    [1,0],    [],        [0,0]]
+silo = [['Red','Blue','Red'],    ['Red','Blue'],    ['Red','Blue'],    [],        ['Blue','Blue']]
 
         #center     #left     #right    #leftleft  #rightright
 silo = [[],    [],    [],    [],        []]
@@ -18,20 +18,20 @@ class Decision:
         pr1 = 5
         pr2 = 5
         pr3 = 5
-        if ([1,0] in silo) or ([0,1] in silo):
-            if ([1,0] in silo):
-                pr1 = silo.index([1,0])
-            if ([0,1] in silo):
-                pr2 = silo.index([1,0])
+        if (['Red','Blue'] in silo) or (['Blue','Red'] in silo):
+            if (['Red','Blue'] in silo):
+                pr1 = silo.index(['Red','Blue'])
+            if (['Blue','Red'] in silo):
+                pr2 = silo.index(['Red','Blue'])
             if pr1 < pr2:
-                print(f"[1,0] : {pr1}")
+                print(f"['Red','Blue'] : {pr1}")
                 return pr1
             else:
-                print(f"[0,1] : {pr2}")
+                print(f"['Blue,'Red'] : {pr2}")
                 return pr2
-        elif ([0,0] in silo):
-            pr3 = silo.index([0,0])
-            print(f"[0,0] : {pr3}")
+        elif (['Blue','Blue'] in silo):
+            pr3 = silo.index(['Blue','Blue'])
+            print(f"['Blue','Blue'] : {pr3}")
             return pr3
         else:
             self.shortest_path_state += 1
--- Vision/silo_detect.py
+++ Vision/silo_detect.py
@@ -7,6 +7,30 @@ import imutils
 
 # height, width = 500, 500
 # imgResize = cv2.resize(image,(height,width), interpolation=cv2.INTER_AREA)
+
+def trackbar(roi, original_img):
+   #ดึงค่าจาก Trackber
+    Lowblue  = cv2.getTrackbarPos("LB","Color Trackbar")
+    Lowgreen = cv2.getTrackbarPos("LG","Color Trackbar")
+    Lowred   = cv2.getTrackbarPos("LR","Color Trackbar")
+    Highblue  = cv2.getTrackbarPos("HB","Color Trackbar")
+    Highgreen = cv2.getTrackbarPos("HG","Color Trackbar")
+    Highred   = cv2.getTrackbarPos("HR","Color Trackbar")
+
+    L = [Lowblue, Lowgreen, Lowred]
+    H = [Highblue, Highgreen, Highred]
+    
+    print(L)
+    lower = np.array(L, np.uint8)
+    upper = np.array(H, np.uint8)
+    
+    mask = cv2.inRange(roi, lower, upper)
+    masked_img  = cv2.bitwise_and(original_img, original_img, mask=mask)
+    
+    # cv2.imshow("Color Trackbar",masked_img )
+    # cv2.imshow("Original Image", original_img)
+    return masked_img
+
 def split_rectangle_into_rois(large_rect):
     x, y, width, height = large_rect
     small_height = height // 3 
@@ -36,7 +60,6 @@ def ColorInSilo(roi):
     # x = 38, y = 75
     state = ''
     rois = roi.copy()
-    mask = color_detect_blue(roi)
     cv2.circle(rois,(45,30),1,(0,0,255),3)
     cv2.circle(rois,(65,20),1,(0,0,255),3)
     cv2.circle(rois,(85,20),1,(0,0,255),3)
@@ -60,13 +83,13 @@ def ColorInSilo(roi):
     blue_point = [np.max(mask_blue[46,45]), np.max(mask_blue[56,65]), np.max(mask_blue[56,85]), np.max(mask_blue[46,105]), np.max(mask_blue[30,45]),
            np.max(mask_blue[20,65]), np.max(mask_blue[20,85]), np.max(mask_blue[30,105])]
     if red_point.count(255) > 5:
-        cv2.putText(rois,f"Red",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.3,(0,0,0),1)
+        cv2.putText(rois,f"Red",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255),1)
         state = 'Red'
     elif blue_point.count(255) > 5:
-        cv2.putText(rois,f"Blue",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.3,(0,0,0),1)
+        cv2.putText(rois,f"Blue",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255),1)
         state = 'Blue'
     else:
-        cv2.putText(rois,f"Empty",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.3,(0,0,0),1)
+        cv2.putText(rois,f"Empty",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.3,(255,255,255),1)
         state = 'Empty'
     # roi = cv2.cvtColor(roi,cv2.COLOR_BGR2HSV)
     # h, s, v = cv2.split(roi)
@@ -107,28 +130,48 @@ def display_rois(small_rectangles, frame, n):
     return silo_state
 
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture("D:\\download\\ballonthefloorclip.mov")
+cap = cv2.VideoCapture("C:\\Users\\User\\Pictures\\Camera Roll\\WIN_20240315_17_52_40_Pro.mp4")
+cv2.namedWindow("Color Trackbar")
+
+def display(value):
+    pass
+
+#เริ่มต้นสร้าง Tracker
+cv2.createTrackbar("LR","Color Trackbar",0,255,display)
+cv2.createTrackbar("LG","Color Trackbar",0,255,display)
+cv2.createTrackbar("LB","Color Trackbar",0,255,display)
+cv2.createTrackbar("HR","Color Trackbar",255,255,display)
+cv2.createTrackbar("HG","Color Trackbar",255,255,display)
+cv2.createTrackbar("HB","Color Trackbar",255,255,display)
 while (True):
     ret, frame = cap.read()
     frame = cv2.resize(frame,(1920,1080))
-    # print(frame.shape)
+    ori = frame.copy()
+    frame = trackbar(frame, ori)
     H, W = frame.shape[:2]
     # print(H, W)
     # 1080 1920
-
-    cv2.rectangle(frame,(1280,400),(1430,650),(255,255,0),2)
-    large_rectangle = (1280, 400, 150, 230)
+    x = 1260
+    w = 150
+    y = 530
+    h = 250
+    # cv2.rectangle(frame,(1280,400),(1430,650),(255,255,0),2)
+    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
+    large_rectangle = (x, y, 150, 230)
     small_rectangles4 = split_rectangle_into_rois(large_rectangle)
 
     print(display_rois(small_rectangles4, frame,4))
 
     Bin = ColorInSilo(frame)
+    # frame = color_detect_red(frame)
     # cv2.imshow("Bin", Bin)
-    # cv2.imshow("frame", frame)
+    cv2.imshow("frame", frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
+    #print(frame.shape)
     
     
 cap.release()
